4fb1d7afc7159b1fd1cbe12c81fa80e5c1ba4634
Edward Zhang <643431636@qq.com>
2022-06-30 14:47:04 +0800

    定义API

--- server/api/upload.php
+++ server/api/upload.php
@@ -1,14 +1,15 @@
 <?php
 
 set_include_path("E:\SiteProj\imgbot\server");
+
+define("FILE_API_NAME", "upload_image");
+
 $uploads_dir = get_include_path() . '/images';
 
-foreach ($_FILES as $file_form_name => $item) {
-    if ($item["error"] == UPLOAD_ERR_OK) {
-        $temp_file_path = $item["tmp_name"];
-        $upload_file_name = uniqid() . '.' . pathinfo($item["name"], PATHINFO_EXTENSION);
-        $upload_file_path = "$uploads_dir/$upload_file_name";
-        echo $upload_file_path;
-        move_uploaded_file($temp_file_path, $upload_file_path);
-    }
+if ($_FILES[FILE_API_NAME]["error"] == UPLOAD_ERR_OK) {
+    $temp_file_path = $_FILES[FILE_API_NAME]["tmp_name"];
+    $upload_file_name = uniqid() . '.' . pathinfo($_FILES[FILE_API_NAME]["name"], PATHINFO_EXTENSION);
+    $upload_file_path = "$uploads_dir/$upload_file_name";
+    echo $upload_file_path;
+    move_uploaded_file($temp_file_path, $upload_file_path);
 }
