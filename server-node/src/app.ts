4ac6964c16963bd560616ca76b35506166673c72
Edward Zhang <643431636@qq.com>
2022-07-12 20:27:14 +0800

    使用startApp钩子

--- server-node/src/app.ts
+++ server-node/src/app.ts
@@ -4,5 +4,9 @@ export const IMAGES_DIR_NAME: string = "images";
 export const SERVER_SETTINGS_FILE_PATH: string = "./server.json";
 
 export const app = express();
-app.use("/images", express.static(`${IMAGES_DIR_NAME}`));
 
+export const startApp = (props: { host: string; port: number; upload_dir_name: string }) => {
+    
+  app.listen(props.port, props.host);
+  app.use("/images", express.static(`${props.upload_dir_name}`));
+};
