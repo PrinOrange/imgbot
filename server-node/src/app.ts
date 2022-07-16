fd44906f00a39f4e03190643c0a77ac551197ea6
Edward Zhang <643431636@qq.com>
2022-07-16 13:41:32 +0800

    fix startup script

--- server-node/src/app.ts
+++ server-node/src/app.ts
@@ -3,17 +3,12 @@ import { useUploadRoute } from "./routes/upload-image";
 import colors from "colors";
 import { useImages } from "./routes/images";
 
-export const prepareApp = ()=>{
-
-}
+export const prepareApp = () => {};
 
 export const startApp = (setting: { host: string; port: number; staticImagesDirName: string }) => {
   const app = express();
-
-  useImages(app,setting.staticImagesDirName);
+  useImages(app, setting.staticImagesDirName);
   useUploadRoute(app, setting.host, setting.port, setting.staticImagesDirName);
-
   app.listen(setting.port, setting.host);
-
   console.info(colors.green(`Now the server is work on http://${setting.host}:${setting.port}`));
 };
