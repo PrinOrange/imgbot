724ee6ac2e690a0fc9899d850f0d9834297cda03
Edward Zhang <643431636@qq.com>
2022-07-16 17:27:08 +0800

    add tools' webpack configurations

--- server-node/config/webpack.dev.config.js
+++ server-node/config/webpack.dev.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 
-const WebpackDevelopmentConfiguration = {
+const ApplicationDevelopmentConfiguration = {
   entry: {
     index:"./src/app/index.ts",
   },
@@ -33,4 +33,36 @@ const WebpackDevelopmentConfiguration = {
   },
 };
 
-module.exports = WebpackDevelopmentConfiguration;
+const ToolsDevelopmentConfiguration = {
+  entry:{
+    manage:"./src/tools/manage.ts"
+  },
+  optimization: {
+    minimize: false,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+  target: "node",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "../build/tools"),
+  },
+}
+
+module.exports = [ApplicationDevelopmentConfiguration,ToolsDevelopmentConfiguration];
--- server-node/config/webpack.prod.config.js
+++ server-node/config/webpack.prod.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 
-const WebpackProductionConfiguration = {
+const ApplicationProductionConfiguration = {
   entry: {
     index:"./src/app/index.ts",
   },
@@ -33,4 +33,36 @@ const WebpackProductionConfiguration = {
   },
 };
 
-module.exports = WebpackProductionConfiguration;
+const ToolsProductionConfiguration = {
+  entry: {
+    manage:"./src/tools/manage.ts"
+  },
+  optimization: {
+    minimize: false,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+  target: "node",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "../build/tools"),
+  },
+};
+
+module.exports = [ApplicationProductionConfiguration,ToolsProductionConfiguration];
