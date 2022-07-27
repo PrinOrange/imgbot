92ae8e611fd31bc5ac2bc696d540f2ac092ff4a9
Edward Zhang <643431636@qq.com>
2022-07-27 19:31:22 +0800

    添加一些设置

--- server-node/src/app/models/settings.model.ts
+++ server-node/src/app/models/settings.model.ts
@@ -1,5 +1,14 @@
-
 export interface ServerSettings {
-    port: number;
-    host: string ;
+  port: number;
+  host: string;
+  password: string;
+
+  sizeLimit: number;
+  storeSpace: number;
+
+  mysql_user: string;
+  mysql_host: string;
+  mysql_password: string;
+  mysql_port: number;
+  mysql_database: string;
 }
