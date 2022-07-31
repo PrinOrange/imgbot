a50b451f2545e4c03fc36102945ee513c0716edc
Edward Zhang <643431636@qq.com>
2022-07-31 19:11:24 +0800

    使用typescript 重写脚本

---
+++ server-node/scripts/pack.ts
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const VERSION = process.env.npm_package_version;
+
+const PackageTemplate = {
+  name: 'imgbot',
+  version: VERSION,
+  description: 'IMGBOT - A Image-server software implemented with nodejs.',
+  scripts: {
+    start: 'node ./index.js',
+  },
+};
+
+const DefaultSettingsFileTemplate = {
+  port: 3300,
+  host: 'localhost',
+};
+
+fs.writeFile(
+    './build/server.json',
+    JSON.stringify(DefaultSettingsFileTemplate, null, 2),
+    (err: any) => {
+      if (err) console.error(err);
+    },
+);
+
+fs.writeFile(
+    './build/package.json',
+    JSON.stringify(PackageTemplate, null, 2),
+    (err: any) => {
+      if (err) console.error(err);
+    },
+);
